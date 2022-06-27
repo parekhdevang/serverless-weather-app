5f2faa194a642ce442a86d6c048580027e4942a5
parekhdevang <parekh.de@husky.neu.edu>
2022-06-27 18:15:12 -0400

    add metric unit for celcius

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{((data.main.temp - 26)/(2)).toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
